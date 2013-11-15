8945e9a782403e53baef389ceaee634334fa5b9e
dscholz-tgm <dscholz@student.tgm.ac.at>
2013-11-15 12:03:51 +0100

    Init commit

---
+++ src/scholz/Start.java
@@ -0,0 +1,18 @@
+package scholz;
+
+/**
+ * Startklasse zum Ausführen des JDBC Programmes
+ * @author Dominik
+ * @version 0.1
+ */
+public class Start {
+    
+    /**
+     * Main Methode - Wird beim starten ausgeführt
+     * @param args Die Command-Line Argumente
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
