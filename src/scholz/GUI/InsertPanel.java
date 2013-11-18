30b7896e1f4e9550eea6732854adeb8660044f94
dscholz-tgm <dscholz@student.tgm.ac.at>
2013-11-18 22:53:34 +0100

    InsertPanel

--- src/scholz/GUI/InsertPanel.java
+++ src/scholz/GUI/InsertPanel.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridLayout;
 import java.util.List;
-import javax.swing.BoxLayout;
 import javax.swing.ComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
